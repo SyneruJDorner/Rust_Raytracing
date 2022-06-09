4409e08de4da2c8fb8927336489fa3878fcba651
justindd1994 <justindd1994@gmail.com>
2022-06-09 11:34:33 +0200

    Cleaned up some code as well as removed the calculation of rays from the camera, onto the camera with now a single called function called 'trace'.

--- src/main.rs
+++ src/main.rs
@@ -30,11 +30,11 @@ https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-generating-
 #[path = "raytracing/material/submaterials/metal.rs"] mod metal;
 #[path = "raytracing/material/submaterials/glass.rs"] mod glass;
 
-use map_3d::deg2rad;
+//use map_3d::deg2rad;
 
-use matrix4x4::Matrix4x4 as Matrix4x4;
+//use matrix4x4::Matrix4x4 as Matrix4x4;
 use vec3::Vec3 as Vec3;
-use ray::Ray as Ray;
+//use ray::Ray as Ray;
 use camera::Camera as Camera;
 use hittablelist::HittableList as HittableList;
 use sphere::Sphere as Sphere;
@@ -45,7 +45,6 @@ use material::submaterials::metal::Metal as Metal;
 #[allow(unused_imports)]
 use material::submaterials::glass::Glass as Glass;
 
-#[allow(warnings, unused)]
 fn main()
 {
     //Image
@@ -77,31 +76,28 @@ fn main()
     camera.set_scale(Vec3::new(1.0, 1.0, 1.0));
     camera.set_aspect_ratio(ASPECT_RATIO);
     camera.set_field_of_view(FOV);
+    camera.prepare_ray();
 
     //Render
-    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+    camera.trace(world, IMAGE_WIDTH, IMAGE_HEIGHT, SAMPLES_PER_PIXEL, MAX_DEPTH)
+    // println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    let angle: f32 = (deg2rad((FOV * 0.5).into()) as f64).tan() as f32;
-    let camera_to_world: Matrix4x4 = Matrix4x4::identity();
-    let ray_origin = camera_to_world.multiply_vec3_matrix(camera.transform.position);
-    let mut ray = Ray::new(ray_origin, vec3::Vec3::new(0.0, 0.0, 0.0));
-    ray.set_dir_matrix(camera.transform.world_matrix * camera.transform.local_matrix);
-
-    for y in 0..IMAGE_HEIGHT
-    {
-        for x in 0..IMAGE_WIDTH
-        {
-            let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
-            for _ in 0..SAMPLES_PER_PIXEL
-            {
-                let x = (2.0 * (x as f32 + 0.5) / IMAGE_WIDTH as f32 - 1.0) * ASPECT_RATIO * angle;
-                let y = (1.0 - 2.0 * (y as f32 + 0.5) / IMAGE_HEIGHT as f32) * angle;
-                let dir: Vec3 = ray.dir_matrix.multiply_dir_matrix(Vec3::new(x, y, -1.0)).normalize();
-                ray.direction = dir;
-                let new_color = pixel_color + Ray::calcaulte_ray(&ray, &world, MAX_DEPTH);
-                pixel_color = new_color;
-            }
-            color::write_color(pixel_color, SAMPLES_PER_PIXEL);
-        }
-    }
+    // for y in 0..IMAGE_HEIGHT
+    // {
+    //     for x in 0..IMAGE_WIDTH
+    //     {
+    //         let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
+    //         for _ in 0..SAMPLES_PER_PIXEL
+    //         {
+    //             let angle: f32 = (deg2rad((camera.fov * 0.5).into()) as f64).tan() as f32;
+    //             let x = (2.0 * (x as f32 + 0.5) / IMAGE_WIDTH as f32 - 1.0) * ASPECT_RATIO * angle;
+    //             let y = (1.0 - 2.0 * (y as f32 + 0.5) / IMAGE_HEIGHT as f32) * angle;
+    //             let dir: Vec3 = camera.ray.dir_matrix.multiply_dir_matrix(Vec3::new(x, y, -1.0)).normalize();
+    //             camera.ray.direction = dir;
+    //             let new_color = pixel_color + Ray::calcaulte_ray(&camera.ray, &world, MAX_DEPTH);
+    //             pixel_color = new_color;
+    //         }
+    //         color::write_color(pixel_color, SAMPLES_PER_PIXEL);
+    //     }
+    // }
 }
--- src/raytracing/camera/camera.rs
+++ src/raytracing/camera/camera.rs
@@ -1,7 +1,11 @@
 use crate::vec3::Vec3;
-//use crate::ray::Ray;
+use crate::ray::Ray;
 use crate::transform::Transform;
+use crate::hittablelist::HittableList;
+use crate::color::write_color;
 use crate::utils::degrees_to_radians;
+
+use map_3d::deg2rad;
 
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
 pub struct Camera
@@ -10,6 +14,7 @@ pub struct Camera
     aspect_ratio: f32,
     pub fov: f32,
     world_up: Vec3,
+    pub ray: Ray
 }
 
 impl Camera
@@ -22,8 +27,16 @@ impl Camera
             transform: Transform::new(),
             aspect_ratio:  degrees_to_radians(90.0),
             fov: degrees_to_radians(90.0),
-            world_up: Vec3::new(0.0, 1.0, 0.0)
+            world_up: Vec3::new(0.0, 1.0, 0.0),
+            ray: Ray::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 0.0))
         }
+    }
+
+    pub fn prepare_ray(&mut self)
+    {
+        let ray_origin = self.transform.local_matrix.multiply_vec3_matrix(self.transform.position);
+        self.ray = Ray::new(ray_origin, Vec3::new(0.0, 0.0, 0.0));
+        self.ray.set_dir_matrix(self.transform.world_matrix * self.transform.local_matrix);
     }
 
     #[allow(dead_code)]
@@ -68,5 +81,30 @@ impl Camera
     {
         self.transform.set_scale(scale);
         return *self;
+    }
+
+    //Calculates the ray relative to the cameras position and rotation
+    pub fn trace(&mut self, world: HittableList, width: i32, height: i32, samples_per_pixel: i32, depth: i32)
+    {
+        println!("P3\n{} {}\n255", width, height);
+
+        for y in 0..height
+        {
+            for x in 0..width
+            {
+                let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
+                for _ in 0..samples_per_pixel
+                {
+                    let angle: f32 = (deg2rad((self.fov * 0.5).into()) as f64).tan() as f32;
+                    let x = (2.0 * (x as f32 + 0.5) / width as f32 - 1.0) * self.aspect_ratio * angle;
+                    let y = (1.0 - 2.0 * (y as f32 + 0.5) / height as f32) * angle;
+                    let dir: Vec3 = self.ray.dir_matrix.multiply_dir_matrix(Vec3::new(x, y, -1.0)).normalize();
+                    self.ray.direction = dir;
+                    let new_color = pixel_color + Ray::calcaulte_ray(&self.ray, &world, depth);
+                    pixel_color = new_color;
+                }
+                write_color(pixel_color, samples_per_pixel);
+            }
+        }
     }
 }
